51bd1ca3afbdcd179ca6cdfddb49df89e8f9322e
Praveen Kumar <praveenmaurya91@gmail.com>
2018-07-04 10:59:49 +1000

    changes in home page

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  
   isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
   constructor(private breakpointObserver: BreakpointObserver) {}
 }
